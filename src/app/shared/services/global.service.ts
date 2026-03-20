6e72462d7cf8e5f7da075a73a9bb4108ff4320ca
oooola <oooola@hotmail.com>
2026-03-20 15:44:24 +0100

    Version 1.6.0.0

--- src/app/shared/services/global.service.ts
+++ src/app/shared/services/global.service.ts
@@ -22,7 +22,7 @@ export class GlobalService {
   ) {}
 
   public getBuildVersion():string {
-    return '1.5.0.0';
+    return '1.6.0.0';
   }
 
   // Sparar senaste hämtade användningstider i minnet.
